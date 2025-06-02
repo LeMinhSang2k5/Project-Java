d8b1b2767748d5656880c65afef72c64ba0c4331
Minh Sang <thailand0987654321.llms@gmail.com>
2025-06-02 22:44:24 +0700

    Fix path element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import './App.scss';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/Home/HomePage';
+import NewHealthProfile from './components/HealthProfile/NewHealthProfile';
+import VaccinationHistory from './components/HealthProfile/VaccinationHistory';
 import Header from './components/Header/Header';
 import Footer from './components/Header/Footer';
-import { Outlet, useNavigation } from 'react-router-dom';
 import Loader from './components/Pagination/Loader';
-import { useState, useEffect } from 'react';
+import BlogPage from './pages/BlogPage';
+import './App.scss';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -16,8 +20,9 @@ function App() {
   if (isLoading) {
     return <Loader/>;
   }
+
   return (
-    <>
+    <Router>
       <div className="app-container">
         <div className="header-container">
           <Header />
@@ -25,7 +30,12 @@ function App() {
 
         <div className="main-container">
           <div className="app-content">
-            <Outlet />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/health-profile/new" element={<NewHealthProfile />} />
+              <Route path="/health-profile/vaccination" element={<VaccinationHistory />} />
+            </Routes>
           </div>
         </div>
 
@@ -33,7 +43,7 @@ function App() {
           <Footer />
         </div>
       </div>
-    </>
+    </Router>
   );
 }
 
