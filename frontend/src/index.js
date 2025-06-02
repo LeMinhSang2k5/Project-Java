059b6548e00cf8fad6662d9a236137eeb9b2634b
Minh Sang <thailand0987654321.llms@gmail.com>
2025-06-02 22:45:28 +0700

    Fix warning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import Layout from './Layout';
-import { BrowserRouter } from 'react-router-dom';
+// import Layout from './Layout';
+// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode> 
-    <BrowserRouter>
-      <Layout/>
-    </BrowserRouter>
-  </React.StrictMode> 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
